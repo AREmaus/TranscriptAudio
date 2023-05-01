a239245232db8ca12d08eebfbb11108e0ead0376
André Ribeiro <75427449+AREmaus@users.noreply.github.com>
2023-05-01 10:54:19 -0300

    Update play.py

--- play.py
+++ play.py
@@ -1,13 +1,14 @@
 import vosk
 import pyaudio
 
-# Carrega o modelo de idioma
+# Carrega o modelo de idioma (para mais informações leia o arquivo readme no repositorio)
 #model_path = "C:/Users/Andre/Downloads/vosk-model-pt-fb-v0.1.1-20220516_2113/vosk-model-pt-fb-v0.1.1-20220516_2113"
 model_path = 'C:/Users/Andre/Desktop/bibliotecas/modelsmall'
 
 model = vosk.Model(model_path)
 
-# Reconhecedor de fala
+# Taxa de reconhecimento de fala 16kHz
+# Voce pode tentar aumentar a taxa de reconhecimento (em vosk.KaldiRecognizer) para 44,1kHz ou superior. Se este for o caso, lembre-se de adequar em rate na entrada de audio.open (linhas abaixo).
 rec = vosk.KaldiRecognizer(model, 16000)
 
 # Captura de audio
@@ -16,10 +17,10 @@ stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, 
 
 # Loop principal para capturar e reconhecer a fala em tempo real
 while True:
-    # Leitura do audio
+    # Buffer de leitura do audio
     data = stream.read(8000)
 
-    # Reconhecimento para VOSK
+    # Entrada para VOSK realizar o processamento
     if rec.AcceptWaveform(data):
         # Saida do texto reconhecido
         print(rec.Result())
